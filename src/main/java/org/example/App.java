1338b53695fc9fd5b9d779b2f25662b01d07e9b3
wzf <1454746033@qq.com>
2020-07-03 10:37:37 +0800

    aaa

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,14 +4,13 @@ package org.example;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
         System.out.println("1");
         System.out.println("dev");
         System.out.println("stash01");
         System.out.println("c1");
+        System.out.println("aaa");
     }
 }
